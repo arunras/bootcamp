4d8e1dd103dda057597bca38dbbad678eaa31aeb
Arun <prith000@citymail.cuny.edu>
2020-07-05 23:15:38 -0500

    spring-kafka

--- spring-kafka/src/main/java/arunx/bootcamp/kafka/controller/KafkaController.java
+++ spring-kafka/src/main/java/arunx/bootcamp/kafka/controller/KafkaController.java
@@ -3,10 +3,15 @@ package arunx.bootcamp.kafka.controller;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import arunx.bootcamp.kafka.controller.service.KafkaService;
 
+@RestController
+@RequestMapping("kafka")
 public class KafkaController {
 
 	private final KafkaService kafkaService;
@@ -15,16 +20,18 @@ public class KafkaController {
 		this.kafkaService = kafkaService;
 	}
 	
-	
+	@PostMapping("/publish")
 	public String sendMessage(@RequestParam("message") String message) {
 		kafkaService.sendMessage(message);
 		return "Published successfully!";
 	}
 	
+	/*
 	@Bean
 	public NewTopic adviceTopic() {
 		return new NewTopic("Bootcamp", 3, (short) 1);
 	}
+	*/
 	
 	
 }
